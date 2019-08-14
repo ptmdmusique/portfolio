f63f4d7448673bff59fb6b61d1ae9961191fd142
Duc Phan Thanh <ptmdmusic@gmail.com>
2019-08-14 02:11:28 -0700

    fix header anim with :before and after

--- components/Header.js
+++ components/Header.js
@@ -6,18 +6,18 @@ import SideDrawer from 'components/SideDrawer';
 const fontSize = "sm:text-2xl text-xl lg:text-3xl md:text-2xl xl:text-4xl "
 const buttonCN =
   "font-bold rounded inline-flex justify-center\
-   items-center text-gray-100 hover:text-green-400 topBotomBordersOut "
+   items-center text-gray-100 hover:text-yellow-300 "
 const buttonSpacing = "py-2 px-2 lg:px-8 mx-8 "
-const iconCN = "butt mr-6 text-center " + fontSize;
+const iconCN = "mr-6 text-center " + fontSize;
 const textCN = "text-center align-middle " + fontSize;
 
 const Header = () => {
-  const headerCN = "butt headerContainer h-20 md:h-32 w-full fixed border-b-2 "
+  const headerCN = "headerContainer h-20 md:h-32 w-full fixed border-b-2 "
 
   const HeaderButton = (props) => {
     return (
       <Link href={"/" + props.page}>
-        <button className={buttonCN + buttonSpacing}>
+        <button className={buttonCN + buttonSpacing + "buttonContainer topBottomBordersOut "}>
           <i className={iconCN + props.iconName}></i>
           <p className={textCN}>{props.name}</p>
         </button>
@@ -44,14 +44,14 @@ const Header = () => {
             wrapper.classList.toggle('visibleDrawer');
           }}
           className={buttonCN + " py-2 px-4 mx-4 "}>
-          <i className={"fas fa-bars text-white text-2xl "}></i>
-          <p className="text-2xl ml-4">Menu</p>
+          <i className={"fas fa-bars text-2xl text-white "}></i>
+          <p className="text-2xl ml-4 text-white ">Menu</p>
         </button>
       </div>
 
       <SideDrawer />
 
-      <style jsx>{`
+      <style jsx global>{`
         .headerContainer {
           background-color: rgba(0,0,0,0.6);
   
@@ -62,32 +62,44 @@ const Header = () => {
           z-index: 40; 
         }
 
-        .butt::after, .butt::before {
-          content: "Butt -> ";
-          color: red;
+        .buttonContainer {
+          position: relative;
         }
-
-        .topBottomBordersOut:before, .topBottomBorderOut:after {
+        
+        .topBottomBordersOut::before, .topBottomBordersOut::after {
           position: absolute;
-          left: 0px;
+          left: 0;
           width: 100%;
           height: 2px;
           background: #FFF;
           content: "";
-          transition: all 0.3s;
+          
+          opacity: 0;
+          transition: all 0.3s;        
         }
-        .topBottomBordersOut:before {
-          top: 0px;
+
+        .topBottomBordersOut::before {
+          top: 10px;   
           transform: translateY(10px);
         }
-        .topBotomBordersOut:after {
-          bottom: 0px;
+
+        .topBottomBordersOut::after {
+          bottom: 0px;   
           transform: translateY(-10px);
         }
-        .topBotomBordersOut:hover:before, .topBotomBordersOut:hover:after
-        {
-            opacity: 1;
-            transform: translateY(0px);
+
+        
+        .topBottomBordersOut:hover:after, .topBottomBordersOut:hover:before {
+          background: #cc0000;
+          opacity: 1;
+        }
+
+        .topBottomBordersOut:hover:after {
+          transform: translateY(-20px);
+        }
+
+        .topBottomBordersOut:hover:before {
+          transform: translateY(20px);
         }
       `}</style>
     </div>
